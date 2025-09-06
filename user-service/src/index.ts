8397cb7805db842200aaf13e7bda1bcb2158ed06
Rishi19032003 <bvrishi9@gmail.com>
2025-09-06 11:57:29 +0530

    Main server set up and database handling

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,9 +1,32 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import userRoutes from "./route.js"
+
+dotenv.config()
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string, {
+            dbName: "Spotify"
+        })
+        console.log("MONGODB CONNECTED SUCCESSFULLY")
+    } catch (error) {
+        console.log("Error in the connectDB function", error)
+    }
+}
 
 const app = express()
 
+app.use("/api/v1", userRoutes)
+
+app.get("/", (req, res) => {
+    res.send("Server is working!")
+})
+
 const port = process.env.PORT || 5000
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log(`Server is listening at port ${port}`)
+    connectDB()
 })
