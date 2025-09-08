91c3ba959db5aca84e3f53566bf5426f45b4da83
Rishi19032003 <bvrishi9@gmail.com>
2025-09-08 19:39:22 +0530

    Controller for register and login user

--- user-service/src/controller.ts
+++ user-service/src/controller.ts
@@ -1,9 +1,69 @@
-import type { Request, Response } from "express";
+import { User } from "./model.js";
+import tryCatch from "./trycatch.js";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
 
-export const registerUser = async (req: Request, res: Response) => {
-    try {
-        
-    } catch (error) {
-        
+export const registerUser = tryCatch(async(req, res) => {
+    const { name, email, password } = req.body
+    let user = await User.findOne({ email })
+    
+    if (user) {
+        res.status(400).json({
+            message: "User already exists"
+        })
+
+        return
     }
-}
+
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    user = await User.create({
+        name,
+        email,
+        password: hashPassword
+    })
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SEC as string, {
+        expiresIn: "7d"
+    })
+
+    res.status(201).json({
+        message: "User registered",
+        user,
+        token
+    })
+})
+
+export const loginUser = tryCatch(async (req, res) => {
+    const { email, password } = req.body
+    
+    const user = await User.findOne({ email })
+    
+    if (!user) {
+        res.status(404).json({
+            message: "User does not exist"
+        })
+
+        return 
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        res.status(400).json({
+            message: "Invalid password"
+        })
+
+        return 
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SEC as string, {
+        expiresIn: "7d"
+    })
+
+    res.status(200).json({
+        message: "Logged in",
+        user,
+        token
+    })
+})
